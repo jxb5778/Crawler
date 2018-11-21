1f0a83f9025efa55798fa8a797f55c79f393c22a
bergj <berg.justin93@gmail.com>
2018-11-21 08:06:48 -0500

    Added query from file functions

--- Crawler/crawler_lib.py
+++ Crawler/crawler_lib.py
@@ -62,3 +62,55 @@ def crawler_csv_to_parquet(file, crawler, dest_dir):
     crawler.reader.to_parquet(dest_filename, engine='pyarrow')
 
     return
+
+
+def crawler_parquet_query_from_file(file, crawler, query, value_list_file='', value_column=''):
+    """ Crawler job that reads in parquet files and aggregates query results
+
+    :param file: string file path/ filename that is being operated on
+    :param crawler: Crawler object, attributes used-
+                reader: DataFrame of the current file read into memory
+                query_return: list of DataFrames holding query results
+    :param pandas_query: string that follows pandas query syntax
+    :param value_list_file: string filename for csv to be read into @crawler.filter,
+                            and will be used to query against the data
+    :param value_column: string for the column name of the data you want read from the file
+    :return: None
+    """
+
+    print(file)
+
+    crawler.reader = pd.read_parquet(file, engine='pyarrow')
+
+    if crawler.filter is None:
+        crawler.filter = pd.read_csv(value_list_file)[value_column]
+
+    pandas_query(crawler=crawler, query=query, value_list=crawler.filter)
+
+    return
+
+
+def crawler_csv_query_from_file(file, crawler, query, value_list_file='', value_column=''):
+    """ Crawler job that reads in parquet files and aggregates query results
+
+    :param file: string file path/ filename that is being operated on
+    :param crawler: Crawler object, attributes used-
+                reader: DataFrame of the current file read into memory
+                query_return: list of DataFrames holding query results
+    :param pandas_query: string that follows pandas query syntax
+    :param value_list_file: string filename for csv to be read into @crawler.filter,
+                            and will be used to query against the data
+    :param value_column: string for the column name of the data you want read from the file
+    :return: None
+    """
+
+    print(file)
+
+    crawler.reader = pd.read_csv(file, encoding='latin1')
+
+    if crawler.filter is None:
+        crawler.filter = pd.read_csv(value_list_file)[value_column]
+
+    pandas_query(crawler=crawler, query=query, value_list=crawler.filter)
+
+    return
